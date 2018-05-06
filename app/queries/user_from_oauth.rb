48d711b9e4c770ef570e0939164a8ddffe7eacba
Matt Powell <fauxparse@gmail.com>
2018-05-06 12:10:50 +1200

    connect multiple identities

--- app/queries/user_from_oauth.rb
+++ app/queries/user_from_oauth.rb
@@ -14,19 +14,28 @@ class UserFromOauth
   attr_reader :auth_hash
 
   def identity
-    Identity.find_by(provider: provider, uid: uid) || new_user.identities.first
+    Identity.find_by(provider: provider, uid: uid) || new_identity
   end
 
-  def new_user
-    User.create!(
-      name: info_hash[:name],
-      email: info_hash[:email],
-      identities: [Identity.new(provider: provider, uid: uid)]
-    )
+  def authenticated_user
+    User.find_by(email: email) ||
+      User.create!(name: name, email: email)
+  end
+
+  def new_identity
+    authenticated_user.identities.create!(provider: provider, uid: uid)
   end
 
   def info_hash
     auth_hash[:info]
+  end
+
+  def name
+    info_hash[:name]
+  end
+
+  def email
+    info_hash[:email]
   end
 
   def provider
--- spec/queries/user_from_oauth_spec.rb
+++ spec/queries/user_from_oauth_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe UserFromOauth, type: :query do
       provider: 'facebook',
       uid: '1234567',
       info: {
-        email: 'facebook@example.com',
-        name: 'Facebook User'
+        email: 'user@example.com',
+        name: 'Test User'
       }
     }
   end
@@ -47,6 +47,22 @@ RSpec.describe UserFromOauth, type: :query do
 
       it 'has an attached identity' do
         expect(user.identities.facebook.first).to eq identity
+      end
+    end
+
+    context 'for a new login method for an existing user' do
+      let(:existing_user) { create(:user, email: 'user@example.com') }
+
+      before do
+        existing_user.identities.create!(provider: 'google', uid: '123456')
+      end
+
+      it 'does not create a new user' do
+        expect { user }.not_to change(User, :count)
+      end
+
+      it 'creates a new identity' do
+        expect { user }.to change(Identity, :count).by 1
       end
     end
   end
