900271bdd0de4b0c651cb7da3f0d4836e32c0e46
Matt Powell <fauxparse@gmail.com>
2018-05-22 07:03:26 +1200

    fix broken scope spec

--- spec/queries/submissions_spec.rb
+++ spec/queries/submissions_spec.rb
@@ -4,19 +4,26 @@ require 'rails_helper'
 
 RSpec.describe Submissions, type: :query do
   subject(:query) { Submissions.new(parameters) }
-
-  before do
-    create(:submission)
-    create(:submission).rejected!
-  end
+  let!(:pending) { create(:submission) }
+  let!(:rejected) { create(:submission).tap(&:rejected!) }
 
   context 'without parameters' do
     let(:parameters) { {} }
-    it { is_expected.to have_exactly(2).items }
+
+    it 'defaults to pending' do
+      expect(query).to have_exactly(1).item
+      expect(query).to include(pending)
+      expect(query).not_to include(rejected)
+    end
   end
 
   context 'querying by state' do
-    let(:parameters) { { state: :pending } }
-    it { is_expected.to have_exactly(1).item }
+    let(:parameters) { { state: :rejected } }
+
+    it 'only includes rejected submissions' do
+      expect(query).to have_exactly(1).item
+      expect(query).not_to include(pending)
+      expect(query).to include(rejected)
+    end
   end
 end
